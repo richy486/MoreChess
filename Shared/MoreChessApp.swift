a5d1219ac654c17f7759b6a1fc6e7ae7681f7828
Richard Adem <richy486@gmail.com>
2023-10-10 11:04:45 +1100

    First code commit.

---
+++ Shared/MoreChessApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoreChessApp.swift
+//  Shared
+//
+//  Created by Richard Adem on 10/10/23.
+//
+
+import SwiftUI
+
+@main
+struct MoreChessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
