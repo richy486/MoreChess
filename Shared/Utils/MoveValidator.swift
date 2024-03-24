a3237b40196ec6e2175343e365557c0261755b4e
Richard Adem <richy486@gmail.com>
2024-03-24 12:43:14 +0100

    Removed ability to attack yourself.

--- Shared/Utils/MoveValidator.swift
+++ Shared/Utils/MoveValidator.swift
@@ -9,7 +9,7 @@ import Foundation
 
 struct MoveValidator {
   static func targetGridFrom(dragIndex: GridCoordinate?, gridOffset: GridCoordinate, board: Board, columnCount: Int, rowCount: Int) -> GridCoordinate? {
-    guard let dragIndex, let piece = board[dragIndex.row][dragIndex.column] else {
+    guard let dragIndex, let currentPlayerPiece = board[dragIndex.row][dragIndex.column] else {
       return nil
     }
     
@@ -22,28 +22,34 @@ struct MoveValidator {
     }
     
     // Validate position
-    // TODO: Maybe replace this with a filter so we can use a guard.
-    for validMove in piece.validMoves {
+    guard currentPlayerPiece.validMoves.first(where: { validMove in
       // Check for not `moveDown`.
-      let validMoveRow = piece.player.movingDown ? validMove.row : validMove.row * -1
-      let validMoveColumn = piece.player.movingDown ? validMove.column : validMove.column * -1
+      let validMoveRow = currentPlayerPiece.player.movingDown ? validMove.row : validMove.row * -1
+      let validMoveColumn = currentPlayerPiece.player.movingDown ? validMove.column : validMove.column * -1
       
       // If there are two `Int.max` their absolute values must be equal.
       if abs(validMoveRow) == Int.max && abs(validMoveColumn) == Int.max
           && abs(gridOffset.row) != abs(gridOffset.column) {
-        continue
+        return false
       }
       
-      if (gridOffset.column == validMoveColumn || (gridOffset.column > 0 && validMoveColumn == Int.max) || (gridOffset.column < 0 && validMoveColumn == -Int.max))
-          && (gridOffset.row == validMoveRow || (gridOffset.row > 0 && validMoveRow == Int.max) || (gridOffset.row < 0 && validMoveRow == -Int.max) ) {
-        
-        // TODO: Is target empty space or opponent's space?
-        
-        // Found a valid target
-        return targetPosition
+      // Is there a board position?
+      guard (gridOffset.column == validMoveColumn || (gridOffset.column > 0 && validMoveColumn == Int.max) || (gridOffset.column < 0 && validMoveColumn == -Int.max))
+              && (gridOffset.row == validMoveRow || (gridOffset.row > 0 && validMoveRow == Int.max) || (gridOffset.row < 0 && validMoveRow == -Int.max) ) else {
+        return false
+      }
+      return true
+    }) != nil else {
+      return nil
+    }
+    
+    // If there is a piece in the target position, it has to be the opponent.
+    if let targetPiece = board[targetPosition.row][targetPosition.column] {
+      guard targetPiece.player != currentPlayerPiece.player else {
+        return nil
       }
     }
 
-    return nil
+    return targetPosition
   }
 }
